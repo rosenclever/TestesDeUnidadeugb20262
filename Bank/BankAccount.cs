005fc1d5e88246c6c8f8fc050342dc14d11096d3
rosenclever <rosenclever@gmail.com>
2026-09-01 19:18:31 -0300

    ajuste no namespace

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -1,45 +1,50 @@
-﻿public class BankAccount
+﻿namespace Bank
 {
-    private string m_customerName;
-    private decimal m_balance;
-    public string CustomerName
-    {
-        get { return m_customerName; }
-        set { m_customerName = value; }
-    }
-    public decimal Balance
-    {
-        get { return m_balance; }
-    }
 
-    public BankAccount(string customerName, decimal balance)
-    {
-        m_customerName = customerName;
-        m_balance = balance;
-    }
 
-    public BankAccount() { }
-
-    public void Debit(decimal amount)
+    public class BankAccount
     {
-        if (amount < 0)
+        private string m_customerName;
+        private decimal m_balance;
+        public string CustomerName
         {
-            throw new ArgumentOutOfRangeException("valor ", amount, "O valor do saque deve ser positivo.");
+            get { return m_customerName; }
+            set { m_customerName = value; }
         }
-        if(amount > m_balance)
+        public decimal Balance
         {
-            throw new InvalidOperationException("Saldo insuficiente.");
+            get { return m_balance; }
         }
-        m_balance -= amount;
-    }
 
-    public void Credit(decimal amount)
-    {
-        if (amount < 0)
+        public BankAccount(string customerName, decimal balance)
         {
-            throw new ArgumentOutOfRangeException("valor ", amount, "O valor do depósito deve ser positivo.");
+            m_customerName = customerName;
+            m_balance = balance;
         }
-        m_balance += amount;
+
+        public BankAccount() { }
+
+        public void Debit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("valor ", amount, "O valor do saque deve ser positivo.");
+            }
+            if (amount > m_balance)
+            {
+                throw new InvalidOperationException("Saldo insuficiente.");
+            }
+            m_balance -= amount;
+        }
+
+        public void Credit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("valor ", amount, "O valor do depósito deve ser positivo.");
+            }
+            m_balance += amount;
+        }
     }
 }
 
